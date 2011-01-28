6fed567fa70487257091d71084372d5211b6d267
jack.chen <jack.chen@isobar.com>
2011-01-28 17:24:05 +0800

    Allow admin to restart, check status and delete jobs bump to 1.0.3

--- app/controllers/delayed_job_admin_controller.rb
+++ app/controllers/delayed_job_admin_controller.rb
@@ -2,9 +2,30 @@ class DelayedJobAdminController < ApplicationController
 
   layout 'delayed_job_admin'
 
-  before_filter :delayed_job_admin_authentication
-
   def index
+    unless current_user.is_admin
+      render :nil => true
+    else
+      p @status = params[:current_status]
+    end
+  end
+
+  def restart
+    %x[script/delayed_job restart]
+    @status = %x[script/delayed_job status]
+    redirect_to :action => 'index', :current_status => @status
+  end
+
+  def check_status
+    @status = %x[script/delayed_job status]
+    redirect_to :action => 'index', :current_status => @status
+  end
+
+  def delete
+    if job = Delayed::Job.find(params[:id])
+      job.delete
+    end
+    redirect_to :action => 'index'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  match 'delayed_job_admin' => 'delayed_job_admin#index', :as => 'delayed_job_admin'
+  scope '/:permalink' do
+    match 'delayed_job_admin' => 'delayed_job_admin#index', :as => 'delayed_job_admin'
+    match 'delayed_job_admin/restart' => 'delayed_job_admin#restart', :as => 'delayed_job_admin/restart'
+    match 'delayed_job_admin/check_status' => 'delayed_job_admin#check_status', :as => 'delayed_job_admin/check_status'
+    match 'delayed_job_admin/delete/:id' => 'delayed_job_admin#delete', :as => 'delayed_job_admin/delete'
+  end
 end
+

dc3cfbba722268e86f34a637d63436a4c3296d5d
Sam Dalton <mail@samdalton.co.nz>
2011-09-27 16:42:34 -0700

    put in admin requirement

--- app/controllers/delayed_job_admin_controller.rb
+++ app/controllers/delayed_job_admin_controller.rb
@@ -3,6 +3,10 @@ class DelayedJobAdminController < ApplicationController
   layout 'delayed_job_admin'
 
   def index
+    unless current_user.admin?
+        redirect_to 'http://quid.com' and return
+    end
+
     @status = if params[:current_status].to_s.include?("delayed_job: running")
       params[:current_status].to_s.sub("delayed_job", "Status")
     elsif params[:current_status].to_s.include?("no")
@@ -12,23 +16,23 @@ class DelayedJobAdminController < ApplicationController
     @jobs = Delayed::Job.page(params[:page]).order("run_at desc")
   end
 
-  def restart
-    %x[script/delayed_job restart]
-    @status = %x[script/delayed_job status]
-    redirect_to :action => 'index', :current_status => @status
-  end
+  #def restart
+    #%x[script/delayed_job restart]
+    #@status = %x[script/delayed_job status]
+    #redirect_to :action => 'index', :current_status => @status
+  #end
 
-  def check_status
-    @status = %x[script/delayed_job status]
-    redirect_to :action => 'index', :current_status => @status
-  end
+  #def check_status
+    #@status = %x[script/delayed_job status]
+    #redirect_to :action => 'index', :current_status => @status
+  #end
 
-  def delete
-    if job = Delayed::Job.find(params[:id])
-      job.attempts = 10
-      job.save
-    end
-    redirect_to :action => 'index'
-  end
+  #def delete
+    #if job = Delayed::Job.find(params[:id])
+      #job.attempts = 10
+      #job.save
+    #end
+    #redirect_to :action => 'index'
+  #end
 
 end

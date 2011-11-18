eb10a47655e5ac0e4b25f7fb891118946a055e52
Jeremie Guichard <djeBrest@gmail.com>
2011-11-18 04:15:36 +0100

    Delegate check_status and restart to the application controller

--- app/controllers/delayed_job_admin_controller.rb
+++ app/controllers/delayed_job_admin_controller.rb
@@ -11,28 +11,29 @@ class DelayedJobAdminController < ApplicationController
       params[:current_status].to_s.sub("delayed_job", "Status")
     elsif params[:current_status].to_s.include?("no")
       "Status: Off"
+    else
+      'Not implemented'
     end
 
     @jobs = Delayed::Job.page(params[:page]).order("run_at desc")
   end
 
-  #def restart
-    #%x[script/delayed_job restart]
-    #@status = %x[script/delayed_job status]
-    #redirect_to :action => 'index', :current_status => @status
-  #end
+  def restart
+    delayed_job_admin_restart
+    redirect_to :action => 'index', :current_status => @status
+  end
 
-  #def check_status
-    #@status = %x[script/delayed_job status]
-    #redirect_to :action => 'index', :current_status => @status
-  #end
+  def check_status
+    delayed_job_admin_check_status
+    redirect_to :action => 'index', :current_status => @status
+  end
 
-  #def delete
-    #if job = Delayed::Job.find(params[:id])
-      #job.attempts = 10
-      #job.save
-    #end
-    #redirect_to :action => 'index'
-  #end
+  def delete
+    if job = Delayed::Job.find(params[:id])
+      job.attempts = 10
+      job.save
+    end
+    redirect_to :action => 'index'
+  end
 
 end
